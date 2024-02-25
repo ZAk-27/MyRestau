db24f6513f8e95199fc76bbcc27001711babb2dc
Mefdev <mefdev.pro@gmail.com>
2024-02-25 22:38:11 +0100

    Add RF serializers

--- backend_myRestau/food/serializers.py
+++ backend_myRestau/food/serializers.py
@@ -1,8 +1,24 @@
 
 from rest_framework import serializers
-from .models import food
+from .models import food, Order, OrderItem
+
 
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = food
         fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        read_only_fields = ('subtotal',)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
